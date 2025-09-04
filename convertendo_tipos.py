58c487692a3b77803e27d7597fa1f4d3c68a84a5
Natanael-cloud <natanpaivacosta@gmail.com>
2025-09-04 18:17:54 -0300

    add conversao de tipos em Python

---
+++ convertendo_tipos.py
@@ -0,0 +1,13 @@
+print(int(1.9))  # Converte float para inteiro
+print(int("10"))  # Converte string para inteiro
+print(float(10.10))  # Converte inteiro para float
+print(float(100))  # Converte inteiro para float
+
+
+valor = 10
+valor_str = str(valor)
+print(type(valor))  # Converte inteiro para string
+print(type(valor_str))  # Converte inteiro para string
+
+print(100/2)  # Resultado float
+print(100//2)  # Resultado inteiro
