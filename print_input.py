5456c25cf16a2107e364bbe225b8468aea338761
Natanael-cloud <natanpaivacosta@gmail.com>
2025-09-04 18:18:28 -0300

    working with input e print

---
+++ print_input.py
@@ -0,0 +1,10 @@
+nome = input("informe seu nome: ")
+age = input("informe sua idade: ")
+
+print(Meu nome é {nome} e eu tenho {age} anos.)
+
+"""no terminal vai aparecer:
+informe seu nome: Natanael
+informe sua idade: 30
+Natanael 30
+"""
