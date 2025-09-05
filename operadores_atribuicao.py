28270ab440a7aaa56f349435d3a28ba58589da8f
Natanael-cloud <natanpaivacosta@gmail.com>
2025-09-05 11:49:08 -0300

    trabalhando com operadores de atribuição

---
+++ operadores_atribuicao.py
@@ -0,0 +1,15 @@
+saldo = 467
+saldo += 200  # saldo = saldo + 200
+print(saldo)  # 667
+
+saldo -= 100  # saldo = saldo - 100
+print(saldo)  # 567
+
+saldo *= 2  # saldo = saldo * 2
+print(saldo)  # 1134
+
+saldo /= 3  # saldo = saldo / 3
+print(saldo)  # 378.0   
+
+saldo //= 2  # saldo = saldo // 2
+print(saldo)  # 189.0   
