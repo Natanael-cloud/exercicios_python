99f8dfc98896d6e4c4e2a707cc94efcaae473f4f
Natanael-cloud <natanpaivacosta@gmail.com>
2025-09-05 14:08:00 -0300

    trabalhando ternario

---
+++ estrutura_condicional_e_repeticao/if_ternario.py
@@ -0,0 +1,9 @@
+saldo = 2000
+saque = 500
+
+status = "Sucesso" if saldo >= saque else "Falha"
+print(f"Saque: {status} ao realizar o saque")
+
+# Exemplo de execução
+# Saque: Sucesso ao realizar o saque
+# Saque: Falha ao realizar o saque
