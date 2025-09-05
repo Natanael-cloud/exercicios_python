36a789b6f6eae28b9cbeeebf3acca58c35ce0b48
Natanael-cloud <natanpaivacosta@gmail.com>
2025-09-05 12:45:35 -0300

    trabalhando estrutra condicionais e repeticao

---
+++ estrutura_condicional_e_repeticao/identacao_blocos.py
@@ -0,0 +1,18 @@
+def sacar (valor):
+    saldo = 1000
+
+
+    if saldo >= valor:
+
+        if saldo >= valor:
+            print("valor sacado com sucesso")
+
+
+def depositar(valor):
+    saldo = 1000
+    saldo += valor
+    print("valor depositado com sucesso")
+
+
+sacar(200)
+
