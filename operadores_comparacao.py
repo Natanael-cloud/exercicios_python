3dd27fe8a31002d447e619023990addae392ecd8
Natanael-cloud <natanpaivacosta@gmail.com>
2025-09-05 11:49:41 -0300

    trabalhando com operadores de comparacao

---
+++ operadores_comparacao.py
@@ -0,0 +1,9 @@
+saldo = 467
+saque = 120
+print(saldo >= saque)  # True
+print(saldo < saque)   # False  
+print(saldo == saque)  # False
+print(saldo != saque)  # True
+print(saldo <= saque)  # False
+print(saldo >= saque and saldo == saque)  # False
+print(saldo >= saque or saldo == saque)   # True
