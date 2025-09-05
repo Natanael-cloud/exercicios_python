f7afed6bda1c4dcd8640bccec9f1f16e24e5da66
Natanael-cloud <natanpaivacosta@gmail.com>
2025-09-05 11:47:55 -0300

    trabalhando com operadores aritmeticos

---
+++ operadores_aritmeticos.py
@@ -0,0 +1,26 @@
+#adição
+print(10 + 5)  # Resultado: 15
+#subtração
+print(10 - 5)  # Resultado: 5
+#multiplicação
+print(10 * 5)  # Resultado: 50
+#divisão
+print(10 / 5)  # Resultado: 2.0 
+#divisão inteira
+print(10 // 3)  # Resultado: 3
+#módulo
+print(10 % 3)  # Resultado: 1
+#exponenciação
+print(2 ** 3)  # Resultado: 8
+
+#ordem de precedência: (), **, *, /, //, %, +, - -
+print(10 + 5 * 2)  # Resultado: 20
+print((10 + 5) * 2)  # Resultado: 30
+
+
+produto_1 = 20
+produto_2 = 30
+
+print(produto_1 + produto_2)  # Resultado: 50
+print(produto_1 * 2 + produto_2 / 2)  # Resultado: 55.0
+print((produto_1 + produto_2) * 0.1)  # Resultado: 5.0
