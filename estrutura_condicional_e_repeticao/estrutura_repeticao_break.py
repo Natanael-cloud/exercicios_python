35b00b786129efe07f27aec01bc3b85a11d0439d
Natanael-cloud <natanpaivacosta@gmail.com>
2025-09-08 19:01:28 -0300

    trabalhando com estrtura de repeticao break

---
+++ estrutura_condicional_e_repeticao/estrutura_repeticao_break.py
@@ -0,0 +1,12 @@
+while True:
+    opcao = int(input("Informe um número: "))
+
+    if opcao == 10:
+        break
+
+print(opcao)
+
+#run: Informe um número: 5
+#run: Informe um número: 8 
+#run: Informe um número: 10
+#no numero 10 o laço é finalizado e o programa imprime 10
