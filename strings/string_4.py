fb53ad4e7f8f3199e3c739ccfec99bb2f072b5f4
Natanael-cloud <natanpaivacosta@gmail.com>
2025-09-09 14:14:18 -0300

    trabalhando string multiplas linhas

---
+++ strings/string_4.py
@@ -0,0 +1,31 @@
+nome = "Natanael"
+
+mensagem = f"""
+Meu nome é {nome},
+e estou aprendendo Python.
+"""
+print(mensagem)
+# Saída:
+"""
+Meu nome é Natanael,
+e estou aprendendo Python.
+"""
+
+
+print(""""
+      ======== MENU ========
+      1 - Depositar
+      2 - Sacar
+      3 - Sair
+        Obrigado por usar nosso sistema!!!
+      
+""")
+# Saída:
+"""
+      ======== MENU ========
+      1 - Depositar
+      2 - Sacar
+      3 - Sair
+        Obrigado por usar nosso sistema!!!
+      ========================
+      """
