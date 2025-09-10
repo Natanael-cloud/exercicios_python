1000ebffa75285330b65f39d5d13088917d5b3d9
Natanael-cloud <natanpaivacosta@gmail.com>
2025-09-10 11:55:22 -0300

    paramentros somente por posicao

---
+++ funcoes/parametros_somente_por_posicao.py
@@ -0,0 +1,7 @@
+def criar_carro(modelo, ano, placa, / marca, motor, combustivel):
+    print(modelo, ano, placa, marca, motor, combustivel)
+
+# Chamando a função
+criar_carro("Civic", 2020, "ABC-1234", marca="Honda", motor="2.0", combustivel="Gasolina")
+# Saída:
+# Civic 2020 ABC-1234 Honda 2.0 Gasolina
